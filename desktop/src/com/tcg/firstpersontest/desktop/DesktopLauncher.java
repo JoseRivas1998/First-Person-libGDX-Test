04e44e02566241ab5ce777351930a7b097130f68
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-11-14 11:26:09 -0800

    Project Generation

---
+++ desktop/src/com/tcg/firstpersontest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tcg.firstpersontest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tcg.firstpersontest.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
